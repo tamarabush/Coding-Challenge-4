bfda05837a92c924a024ba4d1a10c6bc452bc541
tamarabush <tamara.abush14@outlook.com>
2024-09-10 19:40:42 -0400

    Create displayProductDetails function

--- inventory_management.js
+++ inventory_management.js
@@ -1,6 +1,6 @@
 //TASK 1 - Create an Inventory Array of Product Objects:
 
-const inventory = [ 
+let inventory = [ 
     {name: "Perfume", price: 55, quantity: 75, lowStockLevel: 20},
     {name: "Body Lotion", price: 25, quantity: 63, lowStockLevel: 15},
     {name: "Candle", price: 30, quantity: 57, lowStockLevel: 10},
@@ -10,3 +10,30 @@ const inventory = [
 // created an array of 5 body care product objects 
 
 console.log(inventory);
+
+
+
+//TASK 2 - Create a Function to Display Product Details:
+
+function displayProductDetails(inventory) {
+
+    //used if/else statement to determine the stock status of a product 
+    let stockStatus; {
+        if (inventory.quantity > inventory.lowStockLevel) 
+            stockStatus = "In Stock"; //if the quantity is greater than the lowStockLevel, than product is in stock
+        else 
+            stockStatus = "Low Stock"//if its not, than that product has a low stock
+    };
+    
+    console.log(`Name: ${inventory.name}`);
+    console.log(`Price: ${inventory.price}`);
+    console.log(`Quantity: ${inventory.quantity}`);
+    console.log(`Stock Status: ${stockStatus}`);
+
+    };
+
+displayProductDetails(inventory[0]);
+//output = Name: Perfume, Price: 55, Quantity: 75, Stock Status: In Stock
+
+
+//TASK 3 - 
