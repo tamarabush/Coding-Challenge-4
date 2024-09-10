05f91469ec6e79cdd04dbddb24ccd7aa8c3fc717
tamarabush <tamara.abush14@outlook.com>
2024-09-10 17:49:19 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,12 @@
+//TASK 1 - Create an Inventory Array of Product Objects:
+
+const inventory = [ 
+    {name: "Perfume", price: 55, quantity: 75, lowStockLevel: 20},
+    {name: "Body Lotion", price: 25, quantity: 63, lowStockLevel: 15},
+    {name: "Candle", price: 30, quantity: 57, lowStockLevel: 10},
+    {name: "Shampoo", price: 26, quantity: 65, lowStockLevel: 10},
+    {name: "Conditoner", price: 28, quantity: 60, lowStockLevel: 20},
+];
+// created an array of 5 body care product objects 
+
+console.log(inventory);
